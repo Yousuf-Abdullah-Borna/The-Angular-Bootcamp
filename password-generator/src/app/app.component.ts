ec88639fb1cabfd22a7b7e56822b25836f2e1a3b
Yousuf-Abdullah-Borna <y26born@siena.edu>
2021-06-30 08:55:32 -0400

    checkbox fully functional

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+
+  includeLetters:boolean =false;
+  includeNumbers:boolean =false;
+  includeSymbols:boolean =false;
+  password:string="";
+  test: string = "";
+
+
+ onChangeUseLetters(){
+
+this.includeLetters = !this.includeLetters;
+
+ }
+
+ onChangeUseNumbers(){
+
+     this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+
+
+  onButtonClick(){
+
+    
+   
+
+    this.password= "SUMAYA YEACIN" ;
+
+    
+
+  }
 }
