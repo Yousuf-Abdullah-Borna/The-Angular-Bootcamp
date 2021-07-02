188e89adccf86bf3eedd68c38e14cbf3a26bcde6
Yousuf-Abdullah-Borna <y26born@siena.edu>
2021-07-02 13:47:37 -0400

    component comunincation works well

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+    title:"neat tree",
+    imageUrl: 'assets/tree.jpeg',
+    username:'nature',
+    content:'I saw this neat tree today'
+  },
+  {
+    title:"Snowy mountain",
+    imageUrl: 'assets/mountain.jpeg',
+    username:'Mountain man',
+    content:'Here is a picture of snowy mountain'
+  },
+  {
+    title:"Biking !",
+    imageUrl: 'assets/biking.jpeg',
+    username:'Roton',
+    content:'I love biking'
+  }
+
+]
 }
